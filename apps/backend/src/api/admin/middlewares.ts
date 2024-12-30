9d493f2e013f9abcdc9016ad52702ac9d773378f
slusarczykmichal <slusarczyk.michal@outlook.com>
2024-12-30 10:02:39 +0100

    feat: add comission system

--- apps/backend/src/api/admin/middlewares.ts
+++ apps/backend/src/api/admin/middlewares.ts
@@ -1,5 +1,9 @@
 import { MiddlewareRoute } from '@medusajs/framework'
 
+import { comissionMiddlewares } from './comission/middlewares'
 import { orderSetsMiddlewares } from './order-sets/middlewares'
 
-export const adminMiddlewares: MiddlewareRoute[] = [...orderSetsMiddlewares]
+export const adminMiddlewares: MiddlewareRoute[] = [
+  ...orderSetsMiddlewares,
+  ...comissionMiddlewares
+]
