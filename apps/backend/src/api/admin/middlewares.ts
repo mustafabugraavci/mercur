8bd4b45f1b6340a446c2e9904d2a24c627b1cdb6
slusarczykmichal <slusarczyk.michal@outlook.com>
2025-01-30 09:42:19 +0100

    add commission admin panel

--- apps/backend/src/api/admin/middlewares.ts
+++ apps/backend/src/api/admin/middlewares.ts
@@ -2,8 +2,10 @@ import { MiddlewareRoute } from '@medusajs/framework'
 
 import { commissionMiddlewares } from './commission/middlewares'
 import { orderSetsMiddlewares } from './order-sets/middlewares'
+import { sellerMiddlewares } from './sellers/middlewares'
 
 export const adminMiddlewares: MiddlewareRoute[] = [
   ...orderSetsMiddlewares,
-  ...commissionMiddlewares
+  ...commissionMiddlewares,
+  ...sellerMiddlewares
 ]
